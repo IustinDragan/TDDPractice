5fcc71cce29e1668aa63b88dfb18d1162c3b951b
IustinDragan <iustindragan@gmail.com>
2022-09-21 11:58:58 +0300

    Add project files.

---
+++ TDDPractice/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDPractice
+{
+    [TestClass]
+    [TestCategory("TDDPractice")]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
